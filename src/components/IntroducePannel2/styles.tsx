293062e1c7315fa640071dd06a0aaebd817203b6
thinhpham <phamthinh30522@gmail.com>
2023-11-22 04:10:47 -0800

    update code

--- src/components/IntroducePannel2/styles.tsx
+++ src/components/IntroducePannel2/styles.tsx
@@ -7,13 +7,13 @@ const IntroducePannel2Wrapper = styled.div(() => [
            ${tw`flex flex-wrap container mx-auto px-[4vw]`} 
         }
         .content-left{
-            ${tw`h-full xl:w-[40%] w-full font-type-1 xl:mx-0 mx-[4vw] mb-[5vh]`}
+            ${tw`h-full xl:w-[40%] w-full font-type-1 xl:mx-0 mb-[5vh]`}
             h1{
-                ${tw`xl:text-[5vw] text-[16vw] font-thin text-white leading-[1]`}
+                ${tw`xl:text-[5vw] text-[12vw] font-thin text-white leading-[1]`}
             }
         }
         .content-right{
-            ${tw`h-full xl:w-[60%] w-full xl:px-0  px-[4vw]`}
+            ${tw`h-full xl:w-[60%] w-full xl:px-0 `}
             &--center {
                 ${tw`w-full rounded-[10px] bg-cover bg-hero-pattern bg-center xl:h-[50vh] h-[30vh]`}
             }
